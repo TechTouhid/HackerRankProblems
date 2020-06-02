2abc6857a1fa739c3293ede56ea813c8892e7a65
Touhidur Rahman <touhidalamin123@gmail.com>
2020-06-02 19:48:05 +0600

    Tuples done by touhid

---
+++ Python/Tuples.py
@@ -0,0 +1,16 @@
+# https://www.hackerrank.com/challenges/python-tuples/problem
+
+"""
+Sample Input 0
+
+2
+1 2
+Sample Output 0
+
+3713081631934410656
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    print(hash(tuple(integer_list)))
